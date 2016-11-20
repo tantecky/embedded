d3742f8c1f8cb3ca7ba45142aecad28bbbd678d2
tantecky <tantecky@seznam.cz>
2016-11-20 22:32:13 +0100

    dalarm: Test MQTT subscribe

--- esp8266/dalarm.py
+++ esp8266/dalarm.py
@@ -1,17 +1,23 @@
-class NetAlarm(object):
+import sys
+import machine
+import network
+import time
+import ubinascii
+import ujson
+from umqtt.robust import MQTTClient
+
+
+class DoorAlarm(object):
 
     def __init__(self):
         self.config = None
+        self.p0 = machine.Pin(2, machine.Pin.OUT)
 
     def load_config(self):
-        from ujson import load
         with open('cfg.json', 'r') as cfg:
-            self.config = load(cfg)
+            self.config = ujson.load(cfg)
 
     def connect_wifi(self):
-        import network
-        import time
-
         ap_if = network.WLAN(network.AP_IF)
         ap_if.active(False)
 
@@ -27,15 +33,34 @@ class NetAlarm(object):
             time.sleep(1)
 
             if sta_if.isconnected():
-                return True
+                return
 
         print('Unable to connect to AP: {}'.format(ssid))
-        return False
+        sys.exit(1)
+
+    def mqtt_callback(self, topic, msg):
+        self.p0.value(not self.p0.value())
+
+    def mqtt_subscribe(self):
+        client_id = b'tom_' + ubinascii.hexlify(machine.unique_id())
+        self.mqtt = MQTTClient(client_id, self.config['host'],
+                               port=int(self.config['port']))
+
+        self.mqtt.set_callback(self.mqtt_callback)
+        self.mqtt.connect()
+        self.mqtt.subscribe(self.config['topic'])
 
 
 def main():
-    alarm = NetAlarm()
+    alarm = DoorAlarm()
     alarm.load_config()
     alarm.connect_wifi()
+    alarm.mqtt_subscribe()
+
+    while True:
+        # Blocking wait for message
+        alarm.mqtt.wait_msg()
+
+    alarm.mqtt.disconnect()
 
 main()
