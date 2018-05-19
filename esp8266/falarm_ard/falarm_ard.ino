fb6c54785ee9973a5221d4eae6ffb55e0d452c28
Tomas Antecky <tomas@antecky.cz>
2018-05-19 12:57:13 +0200

    falarm_ard: Initial commit

---
+++ esp8266/falarm_ard/falarm_ard.ino
@@ -0,0 +1,64 @@
+#include <limits.h>
+
+const int LED = 2; // D4
+const int REED_PINS[] = { 14, 13 }; // D5, D7
+const int PIN_PIEZO = 12; // D6
+const unsigned long ALARM_DELAY = 6000;
+unsigned long whenDisconnected = 0;
+
+void setup()
+{
+  pinMode(LED, OUTPUT);
+  pinMode(PIN_PIEZO, OUTPUT);
+
+  for (int reed : REED_PINS) {
+    pinMode(reed, INPUT_PULLUP);
+  }
+}
+
+inline bool isConnected()
+{
+  for (int reed : REED_PINS) {
+    if (digitalRead(reed) == HIGH) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+void loop()
+{
+  if (!isConnected()) {
+    digitalWrite(LED, LOW);
+
+    if (whenDisconnected == 0) {
+      whenDisconnected = millis();
+    } else {
+      unsigned long now = millis();
+      unsigned long diff;
+
+      if (whenDisconnected > now) {
+        diff = ULONG_MAX - whenDisconnected + now;
+      } else {
+        diff = now - whenDisconnected;
+      }
+
+      if (diff > ALARM_DELAY) {
+        while (!isConnected()) {
+          analogWrite(PIN_PIEZO, 230);
+          delay(100);
+          analogWrite(PIN_PIEZO, 0);
+          delay(100);
+        }
+        digitalWrite(LED, HIGH);
+        whenDisconnected = 0;
+      }
+    }
+  } else {
+    digitalWrite(LED, HIGH);
+    whenDisconnected = 0;
+  }
+
+  delay(50);
+}
