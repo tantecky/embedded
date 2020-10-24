da4fdc8a43623103488d7c31a2e38a3c44c6c56c
Tomas Antecky <tomas@antecky.cz>
2020-10-24 19:03:23 +0200

    stm32: Move freertos

--- stm32/genericSTM32F407VGT6/cubePIO/Src/freertos.c
+++ stm32/genericSTM32F407VGT6/cubePIO/Src/freertos.c
@@ -21,7 +21,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "FreeRTOS.h"
 #include "task.h"
-//#include "main.h"
+#include "main.h"
 #include "cmsis_os.h"
 
 /* Private includes ----------------------------------------------------------*/
