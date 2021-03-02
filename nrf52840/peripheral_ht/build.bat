8d1005e89987dd4e109fb2ed64acff4ac3f62b10
Tomas Antecky <tomas@antecky.cz>
2021-03-02 15:42:48 +0100

    nrf5: Enable RTT

--- nrf52840/peripheral_ht/build.bat
+++ nrf52840/peripheral_ht/build.bat
@@ -1,3 +1,4 @@
 @echo off
 call env.bat
 west build -b nrf52840dongle_nrf52840
+pause
--- nrf52840/peripheral_ht/env.bat
+++ nrf52840/peripheral_ht/env.bat
@@ -1,2 +1,2 @@
 call "E:\Programy\ncs\v1.5.0\toolchain\cmd\env.cmd"
-set FLASH_PORT="COM4"
+set FLASH_PORT="COM5"
